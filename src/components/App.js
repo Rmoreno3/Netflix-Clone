2bec17a62939495868c6faef9f700fe7b1a85c08
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2021-04-25 16:18:27 -0400

    Agregando componentes e imagenes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar';
 import Cards from './Cards';
-import Tv from '../images/tv.png';
-import VideoTv from '../images/videoTv.m4v';
-import Tv2 from '../images/tv_2.png';
-import VideoTv2 from '../images/videoTv_2.m4v';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Cards Title="adasd" Description="adadasd" Tv={Tv} Video={VideoTv}/>
-      <Cards Title="adasd" Description="adadasd" Tv={Tv2} Video={VideoTv2}/>
+      <Cards />
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,26 +1,58 @@
 import React from 'react'
 import './styles/Cards.css';
+import Tv from '../images/tv.png';
+import VideoTv from '../images/videoTv.m4v';
+import Phone from '../images/mobile.jpg';
+import PhonePic from '../images/phone_pic.png';
 
-
-function Cards(props){
+class Cards extends React.Component {
+  render() {
     return (
-      <div className="cards">
-        <div className="container">
-          <div className="row">
-            <div className="col cards__info">
-              <h1 className="mb-4">{props.Title}</h1>
-              <h3>{props.Description}</h3>
-            </div>
-            <div className="col cards__tv">
-              <img src={props.Tv} alt=""/>
-              <div className="image__video">
-                <video src={props.Video} autoPlay playsInline muted loop></video>
+      <React.Fragment>
+        <div className="cards__tv mt-2">
+          <div className="container">
+            <div className="row">
+              <div className="col cards__info">
+                <h1 className="mb-4">Disfruta en tu TV.</h1>
+                <h3>Ve en smart TV, PlayStation, Xbox, Chromecast, Apple TV, reproductores de Blu-ray y más.</h3>
+              </div>
+              <div className="col cards__tv">
+                <img src={Tv} alt=""/>
+                <div className="image__video">
+                  <video src={VideoTv} autoPlay playsInline muted loop></video>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+
+        <div className="cards__phone mt-2">
+          <div className="container">
+            <div className="row cards__phone-info">
+              <div className="col cards__info">
+                <h1 className="mb-4">Disfruta en tu TV.</h1>
+                <h3>Ve en smart TV, PlayStation, Xbox, Chromecast, Apple TV, reproductores de Blu-ray y más.</h3>
+              </div>
+              <div className="col img__phone">
+                <img src={Phone} alt=""/>
+                <div className="phone__info">
+                  <div className="img__container-phone m-2">
+                    <img src={PhonePic} alt=""/>
+                  </div>
+                  <div className="info__container mr-2">
+                    <p>Stranger Things</p>
+                    <span>Descargando...</span>
+                  </div>
+                  <div className="phone__gif">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     )
+  }
 }
 
 export default Cards;
